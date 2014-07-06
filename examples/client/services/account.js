a9baeaae39c337e00592baf328b459c58daf76b9
Sahat Yalkabov <sakhat@gmail.com>
2014-07-05 23:11:48 -0400

    Refactored account factory

---
+++ examples/client/services/account.js
@@ -0,0 +1,8 @@
+angular.module('MyApp')
+  .factory('Account', ['$http', '$window', function($http, $window) {
+    return {
+      getUserInfo: function() {
+        return $http.get('/api/me?token=' + $window.localStorage.token);
+      }
+    };
+  }]);
