0e201c4f40092f1739b14c9876de90cc75791ff9
Sahat Yalkabov <sakhat@gmail.com>
2014-07-05 23:13:11 -0400

    Fixed logged in state issue after signing in

--- examples/client/services/auth.js
+++ examples/client/services/auth.js
@@ -1,71 +1,41 @@
 angular.module('MyApp')
-  .factory('Auth', ['$http', '$location', '$rootScope', '$cookieStore', '$alert', '$window',
-    function($http, $location, $rootScope, $cookieStore, $alert, $window) {
+  .factory('Auth', ['$http', '$location', '$rootScope', '$alert', '$window',
+    function($http, $location, $rootScope, $alert, $window) {
       var token = $window.localStorage.token;
-      console.log(token);
-      $rootScope.currentUser = $cookieStore.get('user');
-      $cookieStore.remove('user');
+
+      if (token) {
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        if (Date.now() > payload.exp) {
+          $window.alert('Token has expired');
+          delete $window.localStorage.token;
+        } else {
+          $rootScope.currentUser = payload.prn;
+        }
+      }
 
       return {
         login: function(user) {
-          return $http.post('/api/login', user)
-            .success(function(data, status, headers, config) {
-              $window.localStorage.token = data.token;
-              $rootScope.currentUser = data.user;
-              $location.path('/');
-              $alert({
-                title: 'Cheers!',
-                content: 'You have successfully logged in.',
-                placement: 'top-right',
-                type: 'success',
-                duration: 3
-              });
-            })
-            .error(function() {
-              $window.localStorage.removeItem('token');
-              $alert({
-                title: 'Error!',
-                content: 'Invalid username or password.',
-                placement: 'top-right',
-                type: 'danger',
-                duration: 3
-              });
-            });
+          return $http.post('/api/login', user).success(function(data) {
+            $window.localStorage.token = data.token;
+            var token = $window.localStorage.token;
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            $rootScope.currentUser = payload.prn;
+            $location.path('/');
+          });
         },
         signup: function(user) {
-          return $http.post('/api/signup', user)
-            .success(function() {
-              $location.path('/login');
-
-              $alert({
-                title: 'Congratulations!',
-                content: 'Your account has been created.',
-                placement: 'top-right',
-                type: 'success',
-                duration: 3
-              });
-            })
-            .error(function(response) {
-              $alert({
-                title: 'Error!',
-                content: response.data,
-                placement: 'top-right',
-                type: 'danger',
-                duration: 3
-              });
-            });
+          return $http.post('/api/signup', user).success(function() {
+            $location.path('/login');
+          });
         },
         logout: function() {
-          return $http.get('/api/logout').success(function() {
-            $rootScope.currentUser = null;
-            $cookieStore.remove('user');
-            $alert({
-              content: 'You have been logged out.',
-              placement: 'top-right',
-              type: 'info',
-              duration: 3
-            });
-          });
+          console.log('logging out')
+          delete $window.localStorage.token;
+          $rootScope.currentUser = null;
+          $location.path('/');
         }
       };
     }]);
+
+// fix logout redirect
+// fix not logged in on home page
