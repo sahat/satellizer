e45977650f0bd9f2608b5f00b6cc9803285e1a86
Sahat Yalkabov <sakhat@gmail.com>
2014-07-07 19:39:15 -0400

    Add a few quick test TODOs

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,6 +1,10 @@
+//= authInterceptor tests
+// TODO: create a fake token and store it on localstorage
+// TODO: Make sure it is set on HTTP Headers
+// TODO: Make a fake 401 response and make sure $location is set to /login
+
 describe('service', function() {
   beforeEach(module('ngAuth'));
-
 
   describe('version', function() {
     it('should return current version', inject(function(ngAuth) {
