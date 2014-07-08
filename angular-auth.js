5f9f7326060a65b142da4b44ef86af472f1bb6f5
Sahat Yalkabov <sakhat@gmail.com>
2014-07-07 23:28:06 -0400

    Merged Auth factory into ngAuth module

--- angular-auth.js
+++ angular-auth.js
@@ -31,6 +31,42 @@ angular.module('ngAuth', [])
       };
     }];
   })
+  .factory('Local', ['$http', '$location', '$rootScope', '$alert', '$window',
+    function($http, $location, $rootScope, $alert, $window) {
+      var token = $window.localStorage.token;
+
+      if (token) {
+        var payload = JSON.parse($window.atob(token.split('.')[1]));
+        if (Date.now() > payload.exp) {
+          $window.alert('Token has expired');
+          delete $window.localStorage.token;
+        } else {
+          $rootScope.currentUser = payload.prn;
+        }
+      }
+
+      return {
+        login: function(user) {
+          return $http.post('/api/login', user).success(function(data) {
+            $window.localStorage.token = data.token;
+            var token = $window.localStorage.token;
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+            $rootScope.currentUser = payload.prn;
+            $location.path('/');
+          });
+        },
+        signup: function(user) {
+          return $http.post('/api/signup', user).success(function() {
+            $location.path('/login');
+          });
+        },
+        logout: function() {
+          delete $window.localStorage.token;
+          $rootScope.currentUser = null;
+          $location.path('/');
+        }
+      };
+    }])
   .factory('authInterceptor', function($q, $window, $location) {
     return {
       request: function(config) {
