e0ff9d771cea2cdbcd42e8ed0cb9f1c18287b76d
Sahat Yalkabov <sakhat@gmail.com>
2014-07-31 22:35:36 -0400

    moved angular-auth.js to lib folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,9 @@ var rename = require("gulp-rename");
 var uglify = require('gulp-uglify');
 
 gulp.task('default', function () {
-  return gulp.src('angular-auth.js')
+  return gulp.src('lib/angular-auth.js')
     .pipe(ngmin())
     .pipe(uglify())
-    .pipe(rename('angular-auth.min.js'))
+    .pipe(rename('lib/angular-auth.min.js'))
     .pipe(gulp.dest('.'));
 });
