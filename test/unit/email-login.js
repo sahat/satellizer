0468abe6c7ec3bb61889e8f892cf7e36f56c89f9
Sahat Yalkabov <sakhat@gmail.com>
2014-08-05 16:58:07 -0400

    added a test that makes a fake HTTP request to /auth/login

--- test/unit/email-login.js
+++ test/unit/email-login.js
@@ -1,15 +1,41 @@
-describe('Configuration', function() {
+describe('Local sign-in via email', function() {
   beforeEach(module('ngAuth'));
+
+//  afterEach(function() {
+//    $httpBackend.verifyNoOutstandingExpectation();
+//    $httpBackend.verifyNoOutstandingRequest();
+//  });
 
   it('should have a login function', inject(function($auth) {
     expect(angular.isFunction($auth.login)).toBe(true);
   }));
 
-  it('should have a login function', inject(function($auth) {
-    var loginWithoutParams = function() {
+  it('should throw an error without params', inject(function($auth) {
+    var login = function() {
       $auth.login();
     };
-    expect(loginWithoutParams).toThrow();
+    expect(login).toThrow();
+  }));
+
+  it('should return a current user', inject(function($httpBackend, $auth) {
+    var result;
+    $httpBackend.expectPOST('/auth/login').respond({
+      token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7Il9pZCI6IjUzZTEzYjIxMzczNjA2NjgwMjhjOWU3OCIsImVtYWlsIjoic2FoYXRAbWUuY29tIiwicGFzc3dvcmQiOiIkMmEkMTAkQVZTVFZmS3kyZDFFeUFnYWFYVTlydUdKTlplUUNjS1RzREJOL1lGUWFVNEpVb0VDdEtULnUiLCJfX3YiOjB9LCJpYXQiOjE0MDcyNzAyNDQ5MDEsImV4cCI6MTQwNzg3NTA0NDkwMn0.hjVTwHh1xjzq5BhLA1bw1iI_STQwhDLFxqyDxZ8mnaY'
+    });
+
+    $auth.login({ email: 'foo@bar.com', password: '1234' }).then(function(response) {
+      result = response;
+    });
+
+    $httpBackend.flush();
+
+    expect(angular.isObject(result)).toBe(true);
+    expect(result).toEqual({
+      _id: '53e13b2137360668028c9e78',
+      email: 'sahat@me.com',
+      password: '$2a$10$AVSTVfKy2d1EyAgaaXU9ruGJNZeQCcKTsDBN/YFQaU4JUoECtKT.u',
+      __v: 0
+    });
   }));
 
 
