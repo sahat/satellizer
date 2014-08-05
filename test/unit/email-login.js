6f9e41a106e7dd72386264ce9888b5c442c1a6db
Sahat Yalkabov <sakhat@gmail.com>
2014-08-05 15:46:02 -0400

    added two new tests to login() function

--- test/unit/email-login.js
+++ test/unit/email-login.js
@@ -1,3 +1,17 @@
-/**
- * Created by Sahat on 8/2/2014.
- */
+describe('Configuration', function() {
+  beforeEach(module('ngAuth'));
+
+  it('should have a login function', inject(function($auth) {
+    expect(angular.isFunction($auth.login)).toBe(true);
+  }));
+
+  it('should have a login function', inject(function($auth) {
+    var loginWithoutParams = function() {
+      $auth.login();
+    };
+    expect(loginWithoutParams).toThrow();
+  }));
+
+
+});
+
