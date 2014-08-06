12e2c5e743fe5da546ab9c236e59847a7f6e3977
Sahat Yalkabov <sakhat@gmail.com>
2014-08-06 11:22:19 -0400

    3 more tests that check if logoutRedirect is a function, throw an error without params or if input is not a string

--- test/unit/configuration.js
+++ test/unit/configuration.js
@@ -15,13 +15,36 @@ describe('Configuration', function() {
     });
   });
 
-  it('should have a config object', inject(function($auth) {
+  it('should have a config object', function() {
     expect(auth).toBeDefined();
-  }));
+  });
 
-  it('should have a setProvider property', inject(function($auth) {
+  it('should have setProvider method', function() {
     expect(authProvider.setProvider).toBeDefined();
-  }));
+  });
+
+  it('should have addProvider method', function() {
+    expect(authProvider.addProvider).toBeDefined();
+  });
+
+  it('should have logoutRedirect method', function() {
+    expect(authProvider.logoutRedirect).toBeDefined();
+    expect(angular.isFunction(authProvider.logoutRedirect)).toBe(true);
+  });
+
+  it('logoutRedirect should throw an error with no params', function() {
+    expect(authProvider.logoutRedirect).toThrow();
+  });
+
+  it('logoutRedirect should only accept a string', function() {
+    var logout = function() {
+      authProvider.logoutRedirect(1234);
+    };
+    expect(logout).toThrow();
+  });
+
+
+
 
 });
 
