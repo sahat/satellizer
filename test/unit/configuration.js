6b958b105434da66d82df9e50bd0be6373c7320b
Sahat Yalkabov <sakhat@gmail.com>
2014-08-06 11:06:59 -0400

    configuration test refactoring

--- test/unit/configuration.js
+++ test/unit/configuration.js
@@ -1,3 +1,27 @@
-/**
- * Created by Sahat on 8/2/2014.
- */
+describe('Configuration', function() {
+
+  var auth;
+  var authProvider;
+
+  beforeEach(module('ngAuth'));
+
+  beforeEach(inject(function($auth) {
+    auth = $auth;
+  }));
+
+  beforeEach(function() {
+    angular.module('ngAuth').config(function($authProvider) {
+      authProvider = $authProvider;
+    });
+  });
+
+  it('should have a config object', inject(function($auth) {
+    expect(auth).toBeDefined();
+  }));
+
+  it('should have a setProvider property', inject(function($auth) {
+    expect(authProvider.setProvider).toBeDefined();
+  }));
+
+});
+
