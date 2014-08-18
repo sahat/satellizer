04cdc224ee7a2a74742eb8f95a2d7f3077b96c33
Sahat Yalkabov <sakhat@gmail.com>
2014-08-18 02:31:16 -0400

    updated static dir path

--- examples/server/node/server.js
+++ examples/server/node/server.js
@@ -61,7 +61,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../../client')));
-app.use(express.static(path.join(__dirname, '../../../lib')));
+app.use(express.static(path.join(__dirname, '../../..')));
 
 app.get('/api/me', ensureAuthenticated, function(req, res) {
   res.send(req.user);
