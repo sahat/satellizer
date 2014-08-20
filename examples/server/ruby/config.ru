2a14db2b919eb7194d5b9da4fdfc7a05dc5e0219
Sahat Yalkabov <sakhat@gmail.com>
2014-08-20 02:23:58 -0400

    ruby: replaced rails with sinatra

--- examples/server/ruby/config.ru
+++ examples/server/ruby/config.ru
@@ -1,4 +1,2 @@
-# This file is used by Rack-based servers to start the application.
-
-require ::File.expand_path('../config/environment',  __FILE__)
-run Rails.application
+require './app'
+run Sinatra::Application
--- examples/server/ruby/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get '/api/me', to: 'users:me'
-  get '/auth/login', to: 'auth#login'
-  get '/auth/signup', to: 'auth#signup'
-  get '/auth/facebook', to: 'auth#facebook'
-  get '/auth/google', to: 'auth#google'
-  get '/auth/linkedin', to: 'auth#linkedin'
-  get '/auth/twitter', to: 'auth#twitter'
-end
