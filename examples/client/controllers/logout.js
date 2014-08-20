c144c3e725529bf231a8247e2c6e44507b49d682
Sahat Yalkabov <sakhat@gmail.com>
2014-08-20 17:06:23 -0400

    client: display alert message after successful logout

--- examples/client/controllers/logout.js
+++ examples/client/controllers/logout.js
@@ -1,4 +1,12 @@
 angular.module('MyApp')
-  .controller('LogoutCtrl', function($auth) {
-    $auth.logout();
+  .controller('LogoutCtrl', function($auth, $alert) {
+    $auth.logout()
+      .then(function() {
+        $alert({
+          content: 'You have been logged out',
+          animation: 'fadeZoomFadeDown',
+          type: 'material',
+          duration: 3
+        });
+      });
   });
