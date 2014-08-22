b1e122029d7e50f1d5751928b95b96d1d1e5ceec
Sahat Yalkabov <sakhat@gmail.com>
2014-08-22 17:46:27 -0400

    display alerts during successful or unssuccessful link/unlink

--- examples/client/controllers/profile.js
+++ examples/client/controllers/profile.js
@@ -1,10 +1,42 @@
 angular.module('MyApp')
-  .controller('ProfileCtrl', function($scope, $auth) {
+  .controller('ProfileCtrl', function($scope, $auth, $alert) {
     $scope.link = function(provider) {
-      $auth.link(provider);
+      $auth.link(provider)
+        .then(function() {
+          $alert({
+            content: 'You have successfully linked ' + provider + ' account',
+            animation: 'fadeZoomFadeDown',
+            type: 'material',
+            duration: 3
+          });
+        })
+        .catch(function() {
+          $alert({
+            content: 'Could not link ' + provider + ' account',
+            animation: 'fadeZoomFadeDown',
+            type: 'material',
+            duration: 3
+          });
+        });
     };
 
     $scope.unlink = function(provider) {
-      $auth.unlink(provider);
+      $auth.unlink(provider)
+        .then(function() {
+          $alert({
+            content: 'You have successfully unlinked ' + provider + ' account',
+            animation: 'fadeZoomFadeDown',
+            type: 'material',
+            duration: 3
+          });
+        })
+        .catch(function() {
+          $alert({
+            content: 'Could not unlink ' + provider + ' account',
+            animation: 'fadeZoomFadeDown',
+            type: 'material',
+            duration: 3
+          });
+        });
     };
   });
