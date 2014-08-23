771ce1f4a387849c67c968f7afd9d9dd6069f9b4
Sahat Yalkabov <sakhat@gmail.com>
2014-08-23 16:14:46 -0400

    added tests for the new Object.defineProperties code

--- test/configSpec.js
+++ test/configSpec.js
@@ -15,8 +15,66 @@ describe('Configuration', function() {
     });
   });
 
-  it('should have a config object', function() {
+  it('should have a $auth object', function() {
     expect(auth).toBeDefined();
+  });
+
+  it('should allow overriding logoutRedirect', function() {
+    authProvider.logoutRedirect = '/new_url';
+    expect(authProvider.logoutRedirect).toEqual('/new_url');
+    authProvider.logoutRedirect = '/';
+  });
+
+  it('should allow overriding loginRedirect', function() {
+    authProvider.loginRedirect = '/new_url';
+    expect(authProvider.loginRedirect).toEqual('/new_url');
+    authProvider.loginRedirect = '/login';
+  });
+
+  it('should allow overriding loginUrl', function() {
+    authProvider.loginUrl = '/api/login';
+    expect(authProvider.loginUrl).toEqual('/api/login');
+    authProvider.loginUrl = '/auth/login';
+  });
+
+  it('should allow overriding signupUrl', function() {
+    authProvider.signupUrl = '/api/signup';
+    expect(authProvider.signupUrl).toEqual('/api/signup');
+    authProvider.signupUrl = '/auth/signup';
+  });
+
+  it('should allow overriding signupRedirect', function() {
+    authProvider.signupRedirect = '/new_url';
+    expect(authProvider.signupRedirect).toEqual('/new_url');
+    authProvider.signupRedirect = '/login';
+  });
+
+  it('should allow overriding loginRoute', function() {
+    authProvider.loginRoute = '/sign_in';
+    expect(authProvider.loginRoute).toEqual('/sign_in');
+    authProvider.loginRoute = '/login';
+  });
+
+  it('should allow overriding signupRoute', function() {
+    authProvider.signupRoute = '/register';
+    expect(authProvider.signupRoute).toEqual('/register');
+    authProvider.signupRoute = '/signup';
+
+  });
+
+  it('should allow overriding user', function() {
+    authProvider.user = 'user';
+    expect(authProvider.user).toEqual('user');
+  });
+
+  it('should allow overriding tokenName', function() {
+    authProvider.tokenName = 'satellizer_jwt';
+    expect(authProvider.tokenName).toEqual('satellizer_jwt');
+  });
+
+  it('should allow overriding unlinkUrl', function() {
+    authProvider.unlinkUrl = '/disconnect';
+    expect(authProvider.unlinkUrl).toEqual('/disconnect');
   });
 
   it('should have facebook method', function() {
@@ -42,6 +100,7 @@ describe('Configuration', function() {
   it('should have oauth2 method', function() {
     expect(authProvider.oauth2).toBeDefined();
   });
+
 
   it('should update a facebook provider with new params', function() {
     authProvider.facebook({
@@ -84,7 +143,7 @@ describe('Configuration', function() {
   it('should add a new oauth1 provider', function() {
     authProvider.oauth1({
       name: 'goodreads',
-      url: '/auth/goodreads',
+      url: '/auth/goodreads'
     });
     expect(angular.isObject(authProvider.providers['goodreads'])).toBe(true);
     expect(authProvider.providers['goodreads'].url).toBe('/auth/goodreads');
