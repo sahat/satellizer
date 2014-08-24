2d9c0d8a1815c21ac1b092603aa066b2572d1989
Sahat Yalkabov <sakhat@gmail.com>
2014-08-24 03:25:26 -0400

    php: facebook login

---
+++ examples/server/php/app/config/secrets.php
@@ -0,0 +1,6 @@
+<?php
+
+
+return array(
+    'FACEBOOK_SECRET' => 'App Secret'
+);
--- examples/server/php/app/controllers/AuthController.php
+++ examples/server/php/app/controllers/AuthController.php
@@ -2,6 +2,10 @@
 
 class AuthController extends BaseController {
 
+    private function createToken($user) {
+        return $user;
+    }
+
     public function login()
     {
         return 'Not implemented';
@@ -17,5 +21,41 @@ class AuthController extends BaseController {
 
         return Response::make(200);
 	}
+
+    public function facebook() {
+        $accessTokenUrl = 'https://graph.facebook.com/oauth/access_token';
+        $graphApiUrl = 'https://graph.facebook.com/me';
+
+        $params = array(
+            'code' => Input::get('code'),
+            'client_id' => Input::get('clientId'),
+            'redirect_uri' => Input::get('redirectUri'),
+            'client_secret' => Config::get('secrets.FACEBOOK_SECRET')
+        );
+
+        $client = new GuzzleHttp\Client();
+        $response1 = $client->get($accessTokenUrl, ['query' => $params]);
+
+        $accessToken = array();
+        parse_str($response1->getBody(), $accessToken);
+
+        $response2 = $client->get($graphApiUrl, ['query' => $accessToken]);
+        $profile = $response2->json();
+
+        $user = User::where('facebook', $profile->id);
+
+        if ($user->exists) {
+            return Response::json(array('token' => $this->createToken($user)));
+        }
+
+        $user = new User;
+        $user->facebook = profile.id;
+        $user->displayName = profile.name;
+        $user->save();
+
+        return Response::json(array('token' => $this->createToken($user)));
+    }
+
+
 
 }
