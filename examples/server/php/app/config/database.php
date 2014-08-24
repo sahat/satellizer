964d80a017e1c8db864f4b44689c946088ccef32
Sahat Yalkabov <sakhat@gmail.com>
2014-08-24 00:44:11 -0400

    php: removed sqlite db, use mysql instead

--- examples/server/php/app/config/database.php
+++ examples/server/php/app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -55,8 +55,8 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
+			'database'  => 'satellizer',
+			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
