a0f502cd518aeaabc9e513ab1d137dc00513e684
Sahat Yalkabov <sakhat@gmail.com>
2014-08-25 22:35:46 -0400

    node: check if user is defined before updating the profile info

--- examples/server/node/server.js
+++ examples/server/node/server.js
@@ -115,6 +115,9 @@ app.get('/api/me', ensureAuthenticated, function(req, res) {
  */
 app.put('/api/me', ensureAuthenticated, function(req, res) {
   User.findById(req.user, function(err, user) {
+    if (!user) {
+      return res.status(400).send({ message: 'User not found' });
+    }
     user.displayName = req.body.displayName || user.displayName;
     user.email = req.body.email || user.email;
     user.save(function(err) {
