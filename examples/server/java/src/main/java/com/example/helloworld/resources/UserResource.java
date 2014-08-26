e5f2b5b9fd27e814067b0f0bf13474e1ae0cdad5
Alice Chen <chiachun78@gmail.com>
2014-08-26 12:02:06 -0400

    UserResource base imeplementation

--- examples/server/java/src/main/java/com/example/helloworld/resources/UserResource.java
+++ examples/server/java/src/main/java/com/example/helloworld/resources/UserResource.java
@@ -1,5 +1,7 @@
 package com.example.helloworld.resources;
 
+import io.dropwizard.hibernate.UnitOfWork;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.PUT;
@@ -8,18 +10,30 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.example.helloworld.core.User;
+import com.example.helloworld.db.UserDAO;
+
 @Path("/me")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class MeResource {
+public class UserResource {
 	
-	@GET
-	public Response getUser() {
-		return Response.ok().build();
+	private final UserDAO userDAO;
+	
+	public UserResource(UserDAO userDAO) {
+		this.userDAO = userDAO;
 	}
-	
+
+	@GET
+	@UnitOfWork
+	public Response getUser(User user) {
+		return Response.ok().entity(user).build();
+	}
+
 	@PUT
-	public Response updateUser() {
+	@UnitOfWork
+	public Response updateUser(User user) {
+		userDAO.save(user);
 		return Response.ok().build();
 	}
 
