14c5881df8e136246ac72057d0d48e7841799b95
Alice Chen <chiachun78@gmail.com>
2014-08-26 11:55:13 -0400

    Added bcrypt and dw-client dependencies in pom, modified Configuration

--- examples/server/java/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
+++ examples/server/java/src/main/java/com/example/helloworld/HelloWorldConfiguration.java
@@ -1,15 +1,20 @@
 package com.example.helloworld;
 
+import com.bazaarvoice.dropwizard.assets.AssetsBundleConfiguration;
+import com.bazaarvoice.dropwizard.assets.AssetsConfiguration;
 import com.example.helloworld.core.Template;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
 import io.dropwizard.Configuration;
+import io.dropwizard.client.JerseyClientConfiguration;
 import io.dropwizard.db.DataSourceFactory;
+
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
-public class HelloWorldConfiguration extends Configuration {
+public class HelloWorldConfiguration extends Configuration implements AssetsBundleConfiguration {
     @NotEmpty
     private String template;
 
@@ -19,7 +24,17 @@ public class HelloWorldConfiguration extends Configuration {
     @Valid
     @NotNull
     private DataSourceFactory database = new DataSourceFactory();
+    
+    @Valid
+    @NotNull
+    @JsonProperty
+    private final AssetsConfiguration assets = new AssetsConfiguration();
 
+    @Valid
+    @NotNull
+    @JsonProperty
+    private JerseyClientConfiguration httpClient = new JerseyClientConfiguration();
+    
     @JsonProperty
     public String getTemplate() {
         return template;
@@ -52,5 +67,14 @@ public class HelloWorldConfiguration extends Configuration {
     @JsonProperty("database")
     public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
         this.database = dataSourceFactory;
+    }
+
+	@Override
+	public AssetsConfiguration getAssetsConfiguration() {
+		return assets;
+	}
+	
+	public JerseyClientConfiguration getJerseyClientConfiguration() {
+        return httpClient;
     }
 }
