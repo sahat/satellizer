64896f6c7889e12890073b2fb7d34ac93d51a22a
Sahat Yalkabov <sakhat@gmail.com>
2014-08-26 17:53:30 -0400

    moved client-side demo's app.js into app folder
    The minimum compromise to get static assets to work with Java's Dropwizard framework.

--- examples/client/app/app.js
+++ examples/client/app/app.js
@@ -2,14 +2,14 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'Satellizer', 'm
   .config(function($routeProvider, $authProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/home.html'
+        templateUrl: '../views/home.html'
       })
       .when('/login', {
-        templateUrl: 'views/login.html',
+        templateUrl: '../views/login.html',
         controller: 'LoginCtrl'
       })
       .when('/signup', {
-        templateUrl: 'views/signup.html',
+        templateUrl: '../views/signup.html',
         controller: 'SignupCtrl'
       })
       .when('/logout', {
@@ -17,28 +17,29 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'Satellizer', 'm
         controller: 'LogoutCtrl'
       })
       .when('/profile', {
-        templateUrl: 'views/profile.html',
+        templateUrl: '../views/profile.html',
         controller: 'ProfileCtrl',
         protected: true
+
       })
       .otherwise({
         redirectTo: '/'
       });
 
     $authProvider.facebook({
-      clientId: '657854390977827'
+      clientId: '624059410963642'
     });
 
     $authProvider.google({
-      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
+      clientId: '836215426053-98eofc7oss4p4oaeahdi2q5c3ocvnp8s.apps.googleusercontent.com'
     });
 
     $authProvider.github({
-      clientId: '0ba2600b1dbdb756688b'
+      clientId: '45ab07066fb6a805ed74'
     });
 
     $authProvider.linkedin({
-      clientId: '77cw786yignpzj'
+      clientId: '77yh0fkkk1gwue'
     });
 
     $authProvider.twitter({
@@ -48,7 +49,7 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'Satellizer', 'm
     $authProvider.oauth2({
       name: 'foursquare',
       url: '/auth/foursquare',
-      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
+      clientId: '52HQZ5C02PUSLPGSPVWBU43AGIA4QPUGQFRTEWKZFAWLTZ0X',
       redirectUri: window.location.origin,
       authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
     });
