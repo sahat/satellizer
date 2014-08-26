ddf2ab20f93705ea9fec2500c6fce10d986fa977
Sahat Yalkabov <sakhat@gmail.com>
2014-08-25 20:09:07 -0400

    removed ngAnnotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 var gulp = require('gulp');
 var rename = require("gulp-rename");
-var ngAnnotate = require('gulp-ng-annotate');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var complexity = require('gulp-complexity');
@@ -8,14 +7,13 @@ var complexity = require('gulp-complexity');
 gulp.task('minify', function() {
   return gulp.src('satellizer.js')
     .pipe(plumber())
-    .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(rename('satellizer.min.js'))
     .pipe(gulp.dest('.'))
 });
 
 gulp.task('copy', function() {
-  return gulp.src('satellizer.js')
+  return gulp.src(['satellizer.js', 'satellizer.min.js'])
     .pipe(gulp.dest('examples/client/vendor'));
 });
 
