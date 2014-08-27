caf181f848f0cd24d5fda1bf3754dd2b79686e25
Sahat Yalkabov <sakhat@gmail.com>
2014-08-27 16:28:01 -0400

    reverted client ids

--- examples/client/app.js
+++ examples/client/app.js
@@ -27,19 +27,19 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'Satellizer', 'm
       });
 
     $authProvider.facebook({
-      clientId: '624059410963642'
+      clientId: '657854390977827'
     });
 
     $authProvider.google({
-      clientId: '836215426053-98eofc7oss4p4oaeahdi2q5c3ocvnp8s.apps.googleusercontent.com'
+      clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
     });
 
     $authProvider.github({
-      clientId: '45ab07066fb6a805ed74'
+      clientId: '0ba2600b1dbdb756688b'
     });
 
     $authProvider.linkedin({
-      clientId: '77yh0fkkk1gwue'
+      clientId: '77cw786yignpzj'
     });
 
     $authProvider.twitter({
@@ -49,7 +49,7 @@ angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'Satellizer', 'm
     $authProvider.oauth2({
       name: 'foursquare',
       url: '/auth/foursquare',
-      clientId: '52HQZ5C02PUSLPGSPVWBU43AGIA4QPUGQFRTEWKZFAWLTZ0X',
+      clientId: 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
       redirectUri: window.location.origin,
       authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
     });
