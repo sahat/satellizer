b6f0b563da9e3f97ff61d1de6e9e3d8eb00a5bc8
Sahat Yalkabov <sakhat@gmail.com>
2014-08-28 10:47:05 -0400

    fixed popup spec

---
+++ test/popup.spec.js
@@ -0,0 +1,41 @@
+describe('satellizer.popup', function() {
+
+  beforeEach(module('satellizer'));
+
+  beforeEach(inject(['$q', '$window', '$interval', 'satellizer.popup', function($q, $window, $interval, popup) {
+    this.$q = $q;
+    this.$window = $window;
+    this.$interval = $interval;
+    this.popup = popup;
+  }]));
+
+  it('should be defined', function() {
+    expect(this.popup).toBeDefined();
+  });
+
+  it('should add left and top offset options', function() {
+    var options = { width: 481, height: 269 };
+    var preparedOptions = this.popup.prepareOptions(options);
+    expect(preparedOptions.left).toBeDefined();
+    expect(preparedOptions.top).toBeDefined();
+  });
+
+  it('should stringify popup options', function() {
+    var options = { width: 481, height: 269 };
+    var stringOptions = this.popup.stringifyOptions(options);
+    expect(stringOptions).toBe('width=481,height=269');
+  });
+
+  it('should open a new popup', function() {
+    var open = this.popup.open();
+    this.$interval.flush(300);
+    this.$window.postMessage('testing', '*');
+    expect(angular.isObject(open)).toBe(true);
+  });
+
+  it('should postMessage to window', function() {
+    var open = this.popup.open();
+    expect(angular.isObject(open)).toBe(true);
+  });
+});
+
