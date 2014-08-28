cce3670ffd423849276794da34d52e865b5fcdd4
Sahat Yalkabov <sakhat@gmail.com>
2014-08-27 23:30:17 -0400

    added constant service with all config options

--- satellizer.js
+++ satellizer.js
@@ -6,7 +6,6 @@
 
 (function(window, angular, undefined) {
   'use strict';
-
   var config = {
     logoutRedirect: '/',
     loginRedirect: '/',
@@ -86,10 +85,79 @@
 
   angular.module('satellizer', [])
     .constant('satellizer.config', {
-
-    })
-    .constant('satellizer.providers', {
-
+      logoutRedirect: '/',
+      loginRedirect: '/',
+      signupRedirect: '/login',
+      loginUrl: '/auth/login',
+      signupUrl: '/auth/signup',
+      loginRoute: '/login',
+      signupRoute: '/signup',
+      user: 'currentUser', // TODO: remove.
+      tokenName: 'token',
+      tokenPrefix: 'satellizer',
+      unlinkUrl: '/auth/unlink/',
+      providers: {
+        google: {
+          url: '/auth/google',
+          authorizationEndpoint: 'https://accounts.google.com/o/oauth2/auth',
+          redirectUri: window.location.origin,
+          scope: 'openid profile email',
+          scopeDelimiter: ' ',
+          requiredUrlParams: ['scope'],
+          optionalUrlParams: ['display'],
+          display: 'popup',
+          type: '2.0',
+          popupOptions: {
+            width: 452,
+            height: 633
+          }
+        },
+        facebook: {
+          url: '/auth/facebook',
+          authorizationEndpoint: 'https://www.facebook.com/dialog/oauth',
+          redirectUri: window.location.origin + '/',
+          scope: 'email',
+          scopeDelimiter: ',',
+          requiredUrlParams: ['display', 'scope'],
+          display: 'popup',
+          type: '2.0',
+          popupOptions: {
+            width: 481,
+            height: 269
+          }
+        },
+        linkedin: {
+          url: '/auth/linkedin',
+          authorizationEndpoint: 'https://www.linkedin.com/uas/oauth2/authorization',
+          redirectUri: window.location.origin,
+          requiredUrlParams: ['state'],
+          scope: [],
+          scopeDelimiter: ' ',
+          state: 'STATE',
+          type: '2.0',
+          popupOptions: {
+            width: 527,
+            height: 582
+          }
+        },
+        github: {
+          name: 'github',
+          url: '/auth/github',
+          authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+          redirectUri: window.location.origin,
+          scope: [],
+          scopeDelimiter: ' ',
+          type: '2.0',
+          popupOptions: {
+            width: 1020,
+            height: 618
+          }
+        },
+        twitter: {
+          url: '/auth/twitter',
+          type: '1.0'
+        }
+      }
     })
     .provider('$auth', function() {
 
