1cd77e8785317aba5b606bd0b7cd3e7572698fb5
Alice Chen <chiachun78@gmail.com>
2014-08-28 23:11:38 -0400

    Added AuthUtils to abstract token stuff

---
+++ examples/server/java/src/main/java/com/example/helloworld/util/AuthUtils.java
@@ -0,0 +1,43 @@
+package com.example.helloworld.util;
+
+import java.text.ParseException;
+
+import org.joda.time.DateTime;
+
+import com.example.helloworld.core.Token;
+import com.nimbusds.jose.JOSEException;
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.JWSSigner;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
+
+public final class AuthUtils {
+	
+	public static final JWSHeader JWT_HEADER = new JWSHeader(JWSAlgorithm.HS256);
+	public static final String AUTH_HEADER_KEY = "Authorization";
+	
+	public static String getToken(String authHeader) {
+		return authHeader.split(" ")[1];
+	}
+	
+	public static String getSubject(String authHeader) throws ParseException {
+		return SignedJWT.parse(getToken(authHeader)).getJWTClaimsSet().getSubject();
+	}
+	
+	public static Token createToken(String host, long sub, String clientSecret) throws JOSEException {
+		JWTClaimsSet claim = new JWTClaimsSet();
+		claim.setSubject(Long.toString(sub));
+		claim.setIssuer(host);
+		claim.setIssueTime(DateTime.now().toDate());
+		claim.setExpirationTime(DateTime.now().plusDays(14).toDate());
+		
+		JWSSigner signer = new MACSigner(clientSecret);
+		SignedJWT jwt = new SignedJWT(JWT_HEADER, claim);
+		jwt.sign(signer);
+		
+		return new Token(jwt.serialize());
+	}
+	
+}
