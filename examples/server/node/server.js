e2e823b7d6641e245f669894e2ae2543b565beb2
Sahat Yalkabov <sakhat@gmail.com>
2014-08-30 00:22:40 -0400

    node: return 200 ok on unlink

--- examples/server/node/server.js
+++ examples/server/node/server.js
@@ -590,15 +590,16 @@ app.post('/auth/foursquare', function(req, res) {
  |--------------------------------------------------------------------------
  */
 
-app.get('/auth/unlink/:provider', ensureAuthenticated, function(req, res, next) {
+app.get('/auth/unlink/:provider', ensureAuthenticated, function(req, res) {
   var provider = req.params.provider;
   User.findById(req.user, function(err, user) {
     if (!user) {
       return res.status(400).send({ message: 'User not found' });
     }
+
     user[provider] = undefined;
     user.save(function(err) {
-      res.send({ token: createToken(req, user) });
+      res.status(200).end();
     });
   });
 });
