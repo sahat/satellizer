f603019e3695e3fc67c02aacecf2fea27b44f610
Sahat Yalkabov <sakhat@gmail.com>
2014-09-21 00:01:04 -0700

    added environment variables to python config

--- examples/server/python/config.py
+++ examples/server/python/config.py
@@ -1,10 +1,12 @@
+import os
+
 DEBUG = True
-TOKEN_SECRET = 'keyboard cat'
-FACEBOOK_SECRET = 'Facebook Client Secret'
-FOURSQUARE_SECRET = 'Foursquare Client Secret'
-GOOGLE_SECRET = 'Google Client Secret'
-LINKEDIN_SECRET = 'LinkedIn Client Secret'
-TWITTER_CONSUMER_KEY = 'Twitter Consumer Secret'
-TWITTER_CONSUMER_SECRET = 'Twitter Consumer Secret'
-TWITTER_CALLBACK_URL = 'http://localhost:3000'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
+TOKEN_SECRET = os.environ.get('SECRET_KEY') or 'hard to guess string''keyboard cat'
+FACEBOOK_SECRET = os.environ.get('FACEBOOK_SECRET') or 'Facebook Client Secret'
+FOURSQUARE_SECRET = os.environ.get('FOURSQUARE_SECRET') or 'Foursquare Client Secret'
+GOOGLE_SECRET = os.environ.get('GOOGLE_SECRET') or 'Google Client Secret'
+LINKEDIN_SECRET = os.environ.get('LINKEDIN_SECRET') or 'LinkedIn Client Secret'
+TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY') or 'Twitter Consumer Secret'
+TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET') or 'Twitter Consumer Secret'
+TWITTER_CALLBACK_URL = os.environ.get('TWITTER_CALLBACK_URL') or 'http://localhost:3000'
+SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
