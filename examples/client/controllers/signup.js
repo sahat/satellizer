07c2d4e9f441255daf1a8e1609c0d957c0ed7b68
Sahat Yalkabov <sakhat@gmail.com>
2014-09-29 19:56:55 -0700

    display error alert if email is already taken
    Fixes #141

--- examples/client/controllers/signup.js
+++ examples/client/controllers/signup.js
@@ -1,10 +1,17 @@
 angular.module('MyApp')
-  .controller('SignupCtrl', function($scope, $auth) {
+  .controller('SignupCtrl', function($scope, $alert, $auth) {
     $scope.signup = function() {
       $auth.signup({
         displayName: $scope.displayName,
         email: $scope.email,
         password: $scope.password
+      }).catch(function(response) {
+        $alert({
+          content: response.data.message,
+          animation: 'fadeZoomFadeDown',
+          type: 'material',
+          duration: 3
+        });
       });
     };
   });
