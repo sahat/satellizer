e2401d1c4ff61e105f27b9c9719c682d033c5ac9
Sahat Yalkabov <sakhat@gmail.com>
2014-11-11 22:02:03 -0800

    updated gulp to include concatenation plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,36 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var rename = require("gulp-rename");
-var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
 var complexity = require('gulp-complexity');
 
+gulp.task('concat', function() {
+  gulp.src(['src/satellizer.js', 'src/*.js'])
+    .pipe(concat('satellizer.js'))
+    .pipe(gulp.dest('dist'))
+});
+
 gulp.task('minify', function() {
-  return gulp.src('satellizer.js')
+  return gulp.src('dist/satellizer.js')
     .pipe(plumber())
     .pipe(uglify())
     .pipe(rename('satellizer.min.js'))
-    .pipe(gulp.dest('.'))
+    .pipe(gulp.dest('dist'))
 });
 
 gulp.task('copy', function() {
-  return gulp.src(['satellizer.js', 'satellizer.min.js'])
+  return gulp.src(['dist/satellizer.js', 'dist/satellizer.min.js'])
     .pipe(gulp.dest('examples/client/vendor'));
 });
 
 gulp.task('complexity', function() {
-  return gulp.src('satellizer.js')
+  return gulp.src('dist/satellizer.js')
     .pipe(complexity());
 });
 
 gulp.task('watch', function() {
-  gulp.watch('satellizer.js', ['copy', 'minify']);
+  gulp.watch('src/*.js', ['concat', 'copy', 'minify']);
 });
 
 gulp.task('php', function() {
@@ -36,4 +43,4 @@ gulp.task('java', function() {
     .pipe(gulp.dest('examples/server/java/src/main/resources/assets'))
 });
 
-gulp.task('default', ['copy', 'minify', 'watch']);
+gulp.task('default', ['concat', 'copy', 'minify', 'watch']);
