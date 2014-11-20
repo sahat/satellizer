6fa3263195a246b1f69354dc47511cc55e8af081
Sahat Yalkabov <sakhat@gmail.com>
2014-11-20 02:19:44 -0800

    node: windows live secret config

--- examples/server/node/config.js
+++ examples/server/node/config.js
@@ -6,6 +6,7 @@ module.exports = {
   GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Client Secret',
   GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
   LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
+  WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret',
   TWITTER_KEY: process.env.TWITTER_KEY || 'Twitter Consumer Key',
   TWITTER_SECRET: process.env.TWITTER_SECRET || 'Twitter Consumer Secret',
   TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'Twitter Callback Url',
