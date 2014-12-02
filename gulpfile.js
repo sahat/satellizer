48d93dea1f521f85cc939d0e8a4dff4835106ca3
Syl <s.valienne@yahoo.fr>
2014-12-02 13:36:47 +0100

    gulpfile: removed IIFE function wrapper and strict mode

--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,43 @@
-(function() {
+var gulp       = require('gulp'),
+    rename     = require('gulp-rename'),
+    uglify     = require('gulp-uglify'),
+    // plumber    = require('gulp-plumber'),
+    complexity = require('gulp-complexity'),
+    header     = require('gulp-header'),
+    pkg        = require('./package.json');
 
-    'use strict';
+// Banner for building
+var banner = ['/**',
+              ' * <%= pkg.name %> - <%= pkg.description %>',
+              ' * @version <%= pkg.version %>',
+              ' * @link <%= pkg.homepage %>',
+              ' * @license <%= pkg.license %>',
+              ' */',
+              ''].join('\n');
 
-    var gulp       = require('gulp'),
-        rename     = require('gulp-rename'),
-        uglify     = require('gulp-uglify'),
-        // plumber    = require('gulp-plumber'),
-        complexity = require('gulp-complexity'),
-        header     = require('gulp-header'),
-        pkg        = require('./package.json');
+gulp.task('minify', function() {
+    return gulp.src('satellizer.js')
+    // .pipe(plumber())
+    .pipe(uglify())
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest('.'));
+});
 
-    // Banner for building
-    var banner = ['/**',
-                  ' * <%= pkg.name %> - <%= pkg.description %>',
-                  ' * @version <%= pkg.version %>',
-                  ' * @link <%= pkg.homepage %>',
-                  ' * @license <%= pkg.license %>',
-                  ' */',
-                  ''].join('\n');
+gulp.task('copy', ['minify'], function() {
+    return gulp.src(['satellizer.js', 'satellizer.min.js'])
+    .pipe(gulp.dest('examples/client/vendor'));
+});
 
-    gulp.task('minify', function() {
-        return gulp.src('satellizer.js')
-        // .pipe(plumber())
-        .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('.'));
-    });
+gulp.task('complexity', function() {
+    return gulp.src('satellizer.js')
+    .pipe(complexity());
+});
 
-    gulp.task('copy', ['minify'], function() {
-        return gulp.src(['satellizer.js', 'satellizer.min.js'])
-        .pipe(gulp.dest('examples/client/vendor'));
-    });
+gulp.task('watch', function() {
+    gulp.watch('src/*.js', ['concat', 'copy', 'minify']);
+});
 
-    gulp.task('complexity', function() {
-        return gulp.src('satellizer.js')
-        .pipe(complexity());
-    });
-
-    gulp.task('watch', function() {
-        gulp.watch('src/*.js', ['concat', 'copy', 'minify']);
-    });
-
-    gulp.task('default', ['copy', 'watch']);
-
-}());
+gulp.task('default', ['copy', 'watch']);
