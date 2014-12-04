86e0f60ed7216eed8677907e3a4283bcc38a5681
Sahat Yalkabov <sakhat@gmail.com>
2014-12-03 17:53:23 -0800

    updated gulpfile / header for minified file

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,39 @@
-var gulp       = require('gulp'),
-    rename     = require('gulp-rename'),
-    uglify     = require('gulp-uglify'),
-    // plumber    = require('gulp-plumber'),
-    complexity = require('gulp-complexity'),
-    header     = require('gulp-header'),
-    pkg        = require('./package.json');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var plumber = require('gulp-plumber');
+var complexity = require('gulp-complexity');
+var header = require('gulp-header');
+var pkg = require('./package.json');
 
-// Banner for building
 var banner = ['/**',
-              ' * <%= pkg.name %> - <%= pkg.description %>',
-              ' * @version <%= pkg.version %>',
-              ' * @link <%= pkg.homepage %>',
-              ' * @license <%= pkg.license %>',
-              ' */',
-              ''].join('\n');
+  ' * Satellizer <%= pkg.version %>',
+  ' * (c) 2014 <%= pkg.author.name %>',
+  ' * License: <%= pkg.license %>',
+  ' */',
+  ''].join('\n');
 
 gulp.task('minify', function() {
-    return gulp.src('satellizer.js')
-    // .pipe(plumber())
+  return gulp.src('satellizer.js')
+    .pipe(plumber())
     .pipe(uglify())
     .pipe(header(banner, { pkg: pkg }))
-    .pipe(rename({
-        suffix: '.min'
-    }))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('.'));
 });
 
 gulp.task('copy', ['minify'], function() {
-    return gulp.src(['satellizer.js', 'satellizer.min.js'])
+  return gulp.src('satellizer.js')
     .pipe(gulp.dest('examples/client/vendor'));
 });
 
 gulp.task('complexity', function() {
-    return gulp.src('satellizer.js')
+  return gulp.src('satellizer.js')
     .pipe(complexity());
 });
 
 gulp.task('watch', function() {
-    gulp.watch('src/*.js', ['concat', 'copy', 'minify']);
+  gulp.watch('satellizer.js', ['copy', 'minify']);
 });
 
 gulp.task('default', ['copy', 'watch']);
