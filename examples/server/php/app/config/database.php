d914f4dfda66c66d23b7f073cd5c53be5e5aec37
Sahat Yalkabov <sakhat@gmail.com>
2015-01-09 00:29:27 -0800

    php: updated database config

--- examples/server/php/app/config/database.php
+++ examples/server/php/app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'test',
+			'database'  => 'satellizer',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
