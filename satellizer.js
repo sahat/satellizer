be24fa29558856f43162a566b83acb979f99a3b8
Crt Zerjal <crtek07@gmail.com>
2015-02-18 11:23:08 +0100

    fixed error

--- satellizer.js
+++ satellizer.js
@@ -620,6 +620,10 @@
               }
             } catch (error) {}
 
+            if (!popupWindow) {
+              $interval.cancel(polling);
+              deferred.reject({ data: 'Provider Popup Blocked' });
+            } else if (popupWindow.closed) {
               $interval.cancel(polling);
               deferred.reject({ data: 'Authorization Failed' });
             }
