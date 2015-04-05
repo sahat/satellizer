3c773a23f043e934e1aace176a020c46cd31a3d6
Fuxian Ding <nekleding@gmail.com>
2015-04-05 10:11:12 +0800

    revert utils

--- satellizer.js
+++ satellizer.js
@@ -255,6 +255,25 @@
 
         return local;
       }])
+    .service('satellizer.utils', function() {
+      this.camelCase = function(name) {
+        return name.replace(/([\:\-\_]+(.))/g, function(_, separator, letter, offset) {
+          return offset ? letter.toUpperCase() : letter;
+        });
+      };
+
+      this.parseQueryString = function(keyValue) {
+        var obj = {}, key, value;
+        angular.forEach((keyValue || '').split('&'), function(keyValue) {
+          if (keyValue) {
+            value = keyValue.split('=');
+            key = decodeURIComponent(value[0]);
+            obj[key] = angular.isDefined(value[1]) ? decodeURIComponent(value[1]) : true;
+          }
+        });
+        return obj;
+      };
+    })
     .factory('satellizer.storage', function() {
       if (supportsLocalStorage()) {
         return {
