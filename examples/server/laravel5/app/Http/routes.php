3a3cdfcbe28241c95bb44a4993a10554f22e1eb0
Sahat Yalkabov <sakhat@gmail.com>
2015-04-18 00:52:36 -0700

    php: updated routes.php

--- examples/server/laravel5/app/Http/routes.php
+++ examples/server/laravel5/app/Http/routes.php
@@ -11,4 +11,20 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+// OAuth, Login and Signup Routes.
+Route::get('auth/twitter', 'AuthController@twitter');
+Route::post('auth/facebook', 'AuthController@facebook');
+Route::post('auth/foursquare', 'AuthController@foursquare');
+Route::post('auth/github', 'AuthController@github');
+Route::post('auth/google', 'AuthController@google');
+Route::post('auth/linkedin', 'AuthController@linkedin');
+Route::post('auth/login', 'AuthController@login');
+Route::post('auth/signup', 'AuthController@signup');
+Route::get('auth/unlink/{provider}', ['middleware' => 'auth', 'uses' => 'AuthController@unlink']);
+
+// API Routes.
+Route::get('api/me', ['middleware' => 'auth', 'uses' => 'UserController@getUser']);
+Route::put('api/me', ['middleware' => 'auth', 'uses' => 'UserController@updateUser']);
+
+// Initialize Angular.js App Route.
+Route::get('/', 'HomeController@index');
