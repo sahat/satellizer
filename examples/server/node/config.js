912cb56a2aa383f840d39704dbc360febad2049a
Sahat Yalkabov <sakhat@gmail.com>
2015-06-08 14:00:02 -0700

    node: updated config.js

--- examples/server/node/config.js
+++ examples/server/node/config.js
@@ -1,14 +1,14 @@
 module.exports = {
   TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
   MONGO_URI: process.env.MONGO_URI || 'localhost',
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '84e952da26fc9e7a12bbc32523efa8a0',
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook Client Secret',
   FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || '70jGjvrUAEOPlawWGPJYw-Dk',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Client Secret',
   GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
   LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
   WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret',
-  TWITTER_KEY: process.env.TWITTER_KEY || 'vdrg4sqxyTPSRdJHKu4UVVdeD',
-  TWITTER_SECRET: process.env.TWITTER_SECRET || 'cUIobhRgRlXsFyObUMg3tBq56EgGSwabmcavQP4fncABvotRMA',
-  TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'http://localhost:3000',
+  TWITTER_KEY: process.env.TWITTER_KEY || 'Twitter Consumer Key',
+  TWITTER_SECRET: process.env.TWITTER_SECRET || 'Twitter Consumer Secret',
+  TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'Twitter Callback URL',
   YAHOO_SECRET: process.env.YAHOO_SECRET || 'Yahoo Client Secret'
 };
