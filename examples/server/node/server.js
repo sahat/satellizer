752d2a0a838287672119aa779b47d0414ba50b3e
Glenn Geenen <glenn@geenentijd.be>
2015-08-02 10:14:20 +0200

    fix review remarks

--- examples/server/node/server.js
+++ examples/server/node/server.js
@@ -212,9 +212,8 @@ app.post('/auth/google', function(req, res) {
 
     // Step 2. Retrieve profile information about the current user.
     request.get({ url: peopleApiUrl, headers: headers, json: true }, function(err, response, profile) {
-
-      if(profile.error) {
-        return res.status(500).send(profile.error);
+      if (profile.error) {
+        return res.status(500).send({message: profile.error.message});
       }
       // Step 3a. Link user accounts.
       if (req.headers.authorization) {
