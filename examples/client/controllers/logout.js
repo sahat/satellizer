f193bbd3db030a94d597f2df70b7b95645d4727a
Sahat Yalkabov <sakhat@gmail.com>
2015-08-14 21:26:00 -0700

    redirect to "/" after logout

--- examples/client/controllers/logout.js
+++ examples/client/controllers/logout.js
@@ -1,8 +1,9 @@
 angular.module('MyApp')
-  .controller('LogoutCtrl', function($auth, toastr) {
+  .controller('LogoutCtrl', function($location, $auth, toastr) {
     if (!$auth.isAuthenticated()) { return; }
     $auth.logout()
       .then(function() {
         toastr.info('You have been logged out');
+        $location.path('/');
       });
   });
