2e5d220d8936a24b71b556502016c7d5226e80e3
Sahat Yalkabov <sakhat@gmail.com>
2015-08-16 15:39:40 -0700

    updated node.js config file

--- examples/server/node/config.js
+++ examples/server/node/config.js
@@ -1,14 +1,17 @@
 module.exports = {
-  TOKEN_SECRET: process.env.TOKEN_SECRET || 'A hard to guess string',
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'JWT Token Secret',
   MONGO_URI: process.env.MONGO_URI || 'localhost',
-  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || 'Facebook Client Secret',
-  FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || 'Foursquare Client Secret',
-  GOOGLE_SECRET: process.env.GOOGLE_SECRET || 'Google Client Secret',
-  GITHUB_SECRET: process.env.GITHUB_SECRET || 'GitHub Client Secret',
-  LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || 'LinkedIn Client Secret',
-  WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || 'Windows Live Secret',
-  TWITTER_KEY: process.env.TWITTER_KEY || 'Twitter Consumer Key',
-  TWITTER_SECRET: process.env.TWITTER_SECRET || 'Twitter Consumer Secret',
-  TWITTER_CALLBACK: process.env.TWITTER_CALLBACK || 'Twitter Callback URL',
-  YAHOO_SECRET: process.env.YAHOO_SECRET || 'Yahoo Client Secret'
+
+  // OAuth 2.0
+  FACEBOOK_SECRET: process.env.FACEBOOK_SECRET || '',
+  FOURSQUARE_SECRET: process.env.FOURSQUARE_SECRET || '',
+  GOOGLE_SECRET: process.env.GOOGLE_SECRET || '',
+  GITHUB_SECRET: process.env.GITHUB_SECRET || '',
+  LINKEDIN_SECRET: process.env.LINKEDIN_SECRET || '',
+  WINDOWS_LIVE_SECRET: process.env.WINDOWS_LIVE_SECRET || '',
+  YAHOO_SECRET: process.env.YAHOO_SECRET || '',
+
+  // OAuth 1.0
+  TWITTER_KEY: process.env.TWITTER_KEY || '',
+  TWITTER_SECRET: process.env.TWITTER_SECRET || ''
 };
