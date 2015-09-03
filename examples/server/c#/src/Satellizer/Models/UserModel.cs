fa285b12ad34e6aa440afa95b844aa7fc5fd234d
Sahat Yalkabov <sakhat@gmail.com>
2015-09-02 21:38:10 -0700

    new c# asp.net 5 web api project

--- examples/server/c#/src/Satellizer/Models/UserModel.cs
+++ examples/server/c#/src/Satellizer/Models/UserModel.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-namespace Satellizer
+namespace Satellizer.Models
 {
     public class User
     {
