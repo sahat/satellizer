31febe3daef12b009e2e4059a4a1056c150b559a
Sahat Yalkabov <sakhat@gmail.com>
2015-11-11 20:39:15 -0500

    refactored login controller error handling

--- examples/client/controllers/login.js
+++ examples/client/controllers/login.js
@@ -3,21 +3,29 @@ angular.module('MyApp')
     $scope.login = function() {
       $auth.login($scope.user)
         .then(function() {
-          toastr.success('You have successfully signed in');
+          toastr.success('You have successfully signed in!');
           $location.path('/');
         })
-        .catch(function(response) {
-          toastr.error(response.data.message, response.status);
+        .catch(function(error) {
+          toastr.error(error.data.message, error.status);
         });
     };
     $scope.authenticate = function(provider) {
       $auth.authenticate(provider)
         .then(function() {
-          toastr.success('You have successfully signed in with ' + provider);
+          toastr.success('You have successfully signed in with ' + provider + '!');
           $location.path('/');
         })
-        .catch(function(response) {
-          toastr.error(response.data.message);
+        .catch(function(error) {
+          if (error.error) {
+            // Popup error - invalid redirect_uri, pressed cancel button, etc.
+            toastr.error(error.error);
+          } else if (error.data) {
+            // HTTP response error from server
+            toastr.error(error.data.message, error.status);
+          } else {
+            toastr.error(error);
+          }
         });
     };
   });
