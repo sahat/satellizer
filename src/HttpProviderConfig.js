b0b3c3ff2531dc6324103d626e2371d8472ce5a4
Sahat Yalkabov <sakhat@gmail.com>
2016-06-16 18:58:28 -0400

    removed compiled files from src directory

--- src/HttpProviderConfig.js
+++
@@ -1,7 +0,0 @@
-export default class HttpProviderConfig {
-    constructor($httpProvider) {
-        this.$httpProvider = $httpProvider;
-        $httpProvider.interceptors.push('satellizerInterceptor');
-    }
-}
-HttpProviderConfig.$inject = ['$httpProvider'];
