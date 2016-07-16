f9078d59496cb6f99732e0ee75ed8840fcd03604
Sahat Yalkabov <sakhat@gmail.com>
2016-07-16 02:13:40 -0700

    converted oauth unit tests to typescript

---
+++ test/oauth.spec.ts
@@ -0,0 +1,72 @@
+import OAuth from '../src/oauth';
+import OAuth1 from '../src/oauth1';
+import OAuth2 from '../src/oauth2';
+import Config from '../src/config';
+import Shared from '../src/shared';
+import Storage from '../src/storage';
+import Popup from '../src/popup';
+
+let httpBackend;
+let config;
+let storage;
+let shared;
+let popup;
+let oauth;
+let oauth1;
+let oauth2;
+
+describe('OAuth', () => {
+
+  beforeEach(angular.mock.inject(($q, $http, $window, $interval, $log, $httpBackend, $timeout) => {
+    httpBackend = $httpBackend;
+    config = new Config();
+    storage = new Storage($window, config);
+    shared = new Shared($q, $window, $log, config, storage);
+    popup = new Popup($interval, $window);
+    oauth1 = new OAuth1($http, $window, config, popup);
+    oauth2 = new OAuth2($http, $window, $timeout, config, popup, storage);
+    oauth = new OAuth($http, config, shared, oauth1, oauth2);
+  }));
+
+  afterEach(function () {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+  describe('unlink()', () => {
+
+    it('should be defined', () => {
+      expect(oauth.unlink).toBeDefined();
+    });
+
+    it('should unlink a provider successfully', () => {
+      let result = null;
+
+      httpBackend.expectPOST(config.unlinkUrl).respond(200);
+
+      oauth.unlink('google').then((response) => {
+        result = response.status;
+      });
+
+      httpBackend.flush();
+
+      expect(result).toBe(200);
+    });
+
+    it('should unlink a provider and get an error', () => {
+      let result = null;
+
+      httpBackend.expectPOST(config.unlinkUrl).respond(400);
+
+      oauth.unlink('facebook').catch((response) => {
+        result = response.status;
+      });
+
+      httpBackend.flush();
+
+      expect(result).toEqual(400);
+    });
+
+  });
+
+});
