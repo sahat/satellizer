073589635d25bfd58d0c40d3c41cef344a99258c
Sahat Yalkabov <sakhat@gmail.com>
2016-07-17 02:34:45 -0700

    include generated satellizer.js in client example

--- examples/client/vendor/satellizer.js
+++ examples/client/vendor/satellizer.js
@@ -1,3 +1,9 @@
+/**
+ * Satellizer 0.15.0
+ * (c) 2016 Sahat Yalkabov 
+ * License: MIT 
+ */
+
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
@@ -899,3 +905,4 @@
     return ng1;
 
 }));
+//# sourceMappingURL=satellizer.js.map
