8a6c1ddb5bd67c1ed276c8e0650335b1f5816f94
Sahat Yalkabov <sakhat@gmail.com>
2016-07-17 00:46:54 -0700

    renamed index.ts to ng1.ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import typescript from 'rollup-plugin-typescript';
 
 export default {
-  entry: 'src/index.ts',
+  entry: 'src/ng1.ts',
   dest: 'dist/satellizer.js',
   format: 'umd',
   moduleName: 'satellizer',
