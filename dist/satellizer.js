9b78e7a99c0897866260ffd9a53ebe444f8ac92c
Sahat Yalkabov <sakhat@gmail.com>
2016-07-17 01:48:03 -0700

    generate source maps and header banner

--- dist/satellizer.js
+++ dist/satellizer.js
@@ -1,3 +1,9 @@
+/**
+ * Satellizer 0.15.0
+ * (c) 2016 Sahat Yalkabov 
+ * License: MIT 
+ */
+
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import typescript from 'rollup-plugin-typescript';
+const version = require('./package.json').version;
 
 export default {
   entry: 'src/ng1.ts',
   dest: 'dist/satellizer.js',
   sourceMap: 'dist/satellizer.map',
+  banner: '/**\n * Satellizer ' + version + '\n * (c) 2016 Sahat Yalkabov \n * License: MIT \n */\n',
   format: 'umd',
   moduleName: 'satellizer',
   plugins: [
